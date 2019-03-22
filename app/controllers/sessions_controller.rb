e36de9d4c7704313c8213231fc975a388b43edc7
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-22 15:44:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete :username
+    session.delete :name
     redirect_to controller: 'application', action: 'hello'
   end
 end
